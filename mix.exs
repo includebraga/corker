2254efc7c8411de6f96346905f96c764afdb2e00
Fernando Mendes <fernando@mendes.codes>
2019-04-10 23:43:17 +0100

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Corker.MixProject do
     [
       app: :corker,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
